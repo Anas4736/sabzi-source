8bbd49bd09987a090903d661bc5fe26a3eaaf51f
1671301009 <mohdanas47369@gmail.com>
2025-10-07 13:22:25 +0530

    commits

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -51,6 +51,8 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
+        <div>
+        </div>
       />
     )
   }
